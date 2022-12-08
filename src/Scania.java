5b5f8c326ac47e96721582b6b49103b5a792f85f
Ghazal <ghazalg@student.chalmers.se>
2022-12-08 14:52:17 +0100

    task 1 ,lab 3

--- src/Scania.java
+++ src/Scania.java
@@ -3,7 +3,7 @@ public class Scania extends Truck {
 
 public Scania(){
 
-    super(2, 162,0,Color.white,"Scania Dump Truck", 0, 0, Direction.NORTH);
+    super(2, 162,0,Color.white,"Scania Dump Truck", 100, 100, Direction.NORTH);
 }
 //turboOn = false;
 
