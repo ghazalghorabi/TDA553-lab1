51258373497e76349a9f0a767c688bdbe86e41ca
hannapanna3 <117651241+hannapanna3@users.noreply.github.com>
2022-12-13 18:20:16 +0100

    Fixed issue 32 with failing tests

--- src/Scania.java
+++ src/Scania.java
@@ -3,7 +3,7 @@ public class Scania extends Truck {
 
 public Scania(){
 
-    super(2, 162,0,Color.white,"Scania Dump Truck", 100, 100, Direction.WEST);
+    super(2, 162,0,Color.white,"Scania Dump Truck", 100, 100, Direction.NORTH);
 }
 //turboOn = false;
 
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -6,7 +6,7 @@ public class Volvo240 extends Car{
     public final static double trimFactor = 1.25; //satisfied with having trim here, instead of in a own class.
     
     public Volvo240(){
-        super(4, 100, 0 ,Color.black, "Volvo240", 0, 0, Direction.EAST);
+        super(4, 100, 0 ,Color.black, "Volvo240", 0, 0, Direction.NORTH);
     }
     
     protected double speedFactor(){
